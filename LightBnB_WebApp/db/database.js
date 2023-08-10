e26dd648ca9144acf926cdc014aba236b98be9dd
ascotlan <antonio.k.scotland@gmail.com>
2023-08-10 00:20:36 +0000

    refactor the getAllReservations function to query the lightbnb database for real reservations associated with a specific user

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -68,7 +68,21 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const values = [guest_id, limit];
+  const queryString = `
+  SELECT reservations.id AS id, properties.*, reservations.start_date,reservations.end_date, AVG(property_reviews.rating) AS average_rating
+  FROM reservations
+  JOIN properties ON properties.id = reservations.property_id
+  JOIN property_reviews ON property_reviews.property_id = properties.id 
+  WHERE reservations.guest_id = $1
+  GROUP BY reservations.id, properties.id
+  ORDER BY reservations.start_date
+  LIMIT $2
+  `;
+
+  return pool
+    .query(queryString, values)
+    .then((reservation) => reservation.rows);
 };
 
 /// Properties
